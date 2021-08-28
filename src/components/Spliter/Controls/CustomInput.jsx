2be9d53c374f0db79f59afbb2d805c304d76faeb
roninJosue <roninjosue88@gmail.com>
2021-08-27 21:25:34 -0600

    Making the design of the app

--- src/components/Spliter/Controls/CustomInput.jsx
+++ src/components/Spliter/Controls/CustomInput.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React from "react"
 import styles from "./../../styles/controls.module.scss"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faDollarSign, faUser} from "@fortawesome/free-solid-svg-icons"
 
 const CustomInput = (props) => {
-  const {label, inputId} = props;
+  const {label, inputId, inputIcon} = props;
   return (
     <div className={styles.controlsInput}>
-      <label htmlFor={inputId}>{label}</label>
-      <input className={styles.controlsInputText} type="text" id={inputId}/>
+      <label className={styles.controlsLabel} htmlFor={inputId}>{label}</label>
+      <div className={styles.controlsGroup}>
+        {
+          inputIcon && <FontAwesomeIcon className={styles.controlsInputIcon}
+                                        icon={(inputIcon === 'dollar') ? faDollarSign : faUser}/>
+        }
+        <input className={styles.controlsInputText} type="text" id={inputId}/>
+      </div>
     </div>
   )
 }
--- src/components/Spliter/Controls/index.jsx
+++ src/components/Spliter/Controls/index.jsx
@@ -5,7 +5,9 @@ import CustomInput from "./CustomInput";
 const Controls = () => {
   return (
     <div className={styles.controls}>
-      <CustomInput label={'Bill'} text={'Hola'} inputId={'bill'} />
+      <CustomInput label={'Bill'}  inputId={'bill'} inputIcon={'dollar'}/>
+      <CustomInput label={'Number of people'} inputId={'user'} inputIcon={'user'}/>
+      <CustomInput label={'Number of people'} inputId={'user'} />
     </div>
   )
 }
