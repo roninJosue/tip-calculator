364242d176dc38b9c7cf95b3081aa581036afeee
roninJosue <roninjosue88@gmail.com>
2021-08-29 16:54:34 -0600

    Button reset completed.

--- src/components/Spliter/Results/index.jsx
+++ src/components/Spliter/Results/index.jsx
@@ -5,15 +5,15 @@ import Button from "../Buttons/Button";
 import useStore from "../../store/store";
 
 const Results = () => {
-  const {calculateAmountTip, calculateTotal} = useStore()
+  const {calculateAmountTip, calculateTotal, reset} = useStore()
   return (
     <div className={styles.results}>
       <div>
         <Screen label={'Tip Amount'} amount={calculateAmountTip()}/>
-        <Screen label={'Tip Amount'} amount={calculateTotal()}/>
+        <Screen label={'Total'} amount={calculateTotal()}/>
       </div>
       <div>
-        <Button second={true} buttonText={'Reset'}/>
+        <Button reset={reset} second={true} buttonText={'Reset'}/>
       </div>
     </div>
   )
