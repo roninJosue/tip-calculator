74ace18cdd8bfe2aa4124b7749833196a9e5607d
roninJosue <roninjosue88@gmail.com>
2021-09-03 22:39:07 -0600

    Using DRY to the buttons.

--- src/components/Spliter/Buttons/Button.jsx
+++ src/components/Spliter/Buttons/Button.jsx
@@ -1,11 +1,10 @@
 import React from "react"
 import styles from './../../styles/buttons.module.scss'
-import useStore from "../../store/store"
 
 const Button = (props) => {
   const {buttonText, dataPercentage, second, reset, updPercentage} = props
 
-  const handleClick = (e) => {
+  const handleClick = () => {
     if (second) {
       reset()
     } else {
@@ -16,7 +15,7 @@ const Button = (props) => {
   return (
     <button
       data-percentage={dataPercentage}
-      onClick={e => handleClick(e.target)}
+      onClick={() => handleClick()}
       className={second ? styles.button + ' ' + styles.button2 : styles.button}>
       {buttonText}
     </button>
--- src/components/Spliter/Buttons/index.jsx
+++ src/components/Spliter/Buttons/index.jsx
@@ -3,16 +3,42 @@ import styles from './../../styles/buttons.module.scss'
 import Button from "./Button";
 import CustomInput from "../Controls/CustomInput";
 import useStore from "../../store/store";
+import {nanoid} from "nanoid";
 
 const ButtonsContainer = () => {
   const updPercentage = useStore((state) => state.updateTipPercentage)
 
+  const buttonObj = [
+    {
+      buttonText: 5,
+      dataPercentage: 0.05
+    },
+    {
+      buttonText: 10,
+      dataPercentage: 0.1
+    },
+    {
+      buttonText: 15,
+      dataPercentage: 0.15
+    },
+    {
+      buttonText: 25,
+      dataPercentage: 0.25
+    },
+    {
+      buttonText: 50,
+      dataPercentage: 0.5
+    }
+  ];
+
   return(<div className={styles.buttonsContainer}>
-    <Button updPercentage={updPercentage} buttonText='5%' dataPercentage={0.05} />
-    <Button updPercentage={updPercentage} buttonText='10%' dataPercentage={0.1} />
-    <Button updPercentage={updPercentage} buttonText='15%' dataPercentage={0.15} />
-    <Button updPercentage={updPercentage} buttonText='25%' dataPercentage={0.25} />
-    <Button updPercentage={updPercentage} buttonText='50%' dataPercentage={0.5} />
+    {
+      buttonObj.map(button => <Button
+        key={nanoid()}
+        updPercentage={updPercentage}
+        buttonText={button.buttonText}
+        dataPercentage={button.dataPercentage}/>)
+    }
     <CustomInput input2={true} updPercentage={updPercentage} placeHolder={'Custom'}/>
   </div>)
 }
